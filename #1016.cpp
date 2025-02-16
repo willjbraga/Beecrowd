5f454ffbbb9768e5f747ddaff041dd2a45f0f2db
willjbraga <williamjubraga@gmail.com>
2025-02-16 17:28:49 -0300

    Create #1016.cpp

---
+++ #1016.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int X;
+
+    cin >> X;
+
+    cout << X*2 << " minutos" << endl;
+}
